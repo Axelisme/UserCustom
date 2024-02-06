3bad0b0a2470744bfa7f80eef7e749a1f43e951c
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-02-06 15:26:20 +0800

    Update Conda initialization in shell setup scripts

--- install_conda.sh
+++ install_conda.sh
@@ -2,4 +2,6 @@
 
 wget -P ~/Downloads/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
 bash ~/Downloads/Miniconda3-latest-Linux-x86_64.sh;
+source $HOME/.bashrc;
+[[ -f $HOME/.zshrc ]] && conda init zsh;
 conda config --set auto_activate_base false
--- setup_shell.sh
+++ setup_shell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # recursive copy, if file exists, backup it
-backup_ln() {
-    local src=$1 dst=$2
+backup_apply() {
+    local src=$2 dst=$3
 
     if [[ ! -e $src ]]; then
         echo "Error: $src not exists"
@@ -12,22 +12,23 @@ backup_ln() {
             mv $dst $dst.bak
         fi
         mkdir -p $dst
-        for file in $(ls $src); do
-            backup_ln $src/$file $dst/$file
+        for file in $(\ls $src); do
+            backup_apply $1 $src/$file $dst/$file
         done
     elif [[ -f $src ]]; then
         if [[ -e $dst ]]; then
             echo "Backup $dst"
             mv $dst $dst.bak
         fi
-        ln $src $dst
+        # $1 is the copy or move command
+        $1 $src $dst
     fi
 }
 
 UserCustom=$(dirname "$0")
-backup_ln $UserCustom/rc/.bashrc ~/.bashrc # setup bashrc
-backup_ln $UserCustom/rc/.zshrc ~/.zshrc   # setup zshrc
-backup_ln $UserCustom/rc/.vimrc ~/.vimrc   # setup vimrc
-backup_ln $UserCustom/.config ~/.config    # setup config
+backup_apply cp $UserCustom/rc/.bashrc ~/.bashrc # setup bashrc
+backup_apply cp $UserCustom/rc/.zshrc ~/.zshrc   # setup zshrc
+backup_apply ln $UserCustom/rc/.vimrc ~/.vimrc   # setup vimrc
+backup_apply ln $UserCustom/.config ~/.config    # setup config
 
 unset backup_ln

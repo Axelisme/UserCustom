c9ed0665e0d790a28d3f2bcf429d4c7bcd8c9e3f
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-02-07 23:29:30 +0800

    Update conda initialization and auto activation settings

--- install_conda.sh
+++ install_conda.sh
@@ -2,6 +2,5 @@
 
 wget -P ~/Downloads/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
 bash ~/Downloads/Miniconda3-latest-Linux-x86_64.sh;
-source $HOME/.bashrc;
-[[ -f $HOME/.zshrc ]] && conda init zsh;
-conda config --set auto_activate_base false
+[[ -f $HOME/.zshrc ]] && bash -i -c 'conda init zsh';
+bash -i -c 'conda config --set auto_activate_base false';

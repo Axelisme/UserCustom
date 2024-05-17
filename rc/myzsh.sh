e69ead7a4c0579b8672b25cb08e7d369046a880a
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-05-17 17:35:54 +0800

    chore: Update bash configuration files, aliases, and package manager commands

--- rc/myzsh.sh
+++ rc/myzsh.sh
@@ -4,37 +4,67 @@ source $USER_CUSTOM/rc/myset.sh
 
 ###################################################
 
-# zxoide
-alias cd='z'
+# ZINIT_HOME
+ZINIT_HOME=/usr/share/zinit
+# Download Zinit, if it's not there yet
+if [ ! -d "$ZINIT_HOME" ]; then
+   ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+   mkdir -p "$(dirname $ZINIT_HOME)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
 
-# zsh
-# 移除重复的命令历史
-setopt HIST_IGNORE_ALL_DUPS
-# 设置可以使用通配符
-setopt nonomatch
-# 有些theme需要設定這才能正常顯示
-setopt promptsubst
 
 # 加载zinit
-source $ZINIT_PATH
+source $ZINIT_HOME/zinit.zsh
 
 # 加载主题
-zinit light romkatv/powerlevel10k
+zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 # 插件
-## 语法高亮
-zinit light zdharma-continuum/fast-syntax-highlighting
-## 补全
+zinit light Aloxaf/fzf-tab
 zinit light zsh-users/zsh-completions
-## 根据子串搜索历史命令
 zinit light zsh-users/zsh-autosuggestions
-## 萬用解壓縮指令
-zinit snippet OMZP::extract
-## 双击esc快速在命令前添加或删除sudo
-zinit snippet OMZP::sudo
-## git插件
+zinit light zdharma-continuum/fast-syntax-highlighting
 zinit snippet OMZP::git
-## 加载 OMZ 框架及
-zinit snippet OMZL::completion.zsh
-zinit snippet OMZL::history.zsh
-zinit snippet OMZL::key-bindings.zsh
+zinit snippet OMZP::sudo
+zinit snippet OMZP::extract
+# zinit snippet OMZL::key-bindings.zsh
+zinit snippet OMZP::command-not-found
+
+# Load completions
+autoload -Uz compinit && compinit
+zinit cdreplay -q
+
+# Keybindings
+bindkey -e
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+bindkey '\033[H' beginning-of-line
+bindkey '\033[F' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
+
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+# Shell integrations
+type fzf > /dev/null && eval "$(fzf --zsh)"
+type zoxide > /dev/null && eval "$(zoxide init zsh)" && alias cd='z'

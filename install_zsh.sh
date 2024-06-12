6627b9245eb6d56ac1e6129529f13afd413712cb
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-06-13 02:45:23 +0800

    add fallback ps1

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,6 +3,4 @@
 cd ~/.local/bin
 wget -O zsh_installer https://raw.githubusercontent.com/romkatv/zsh-bin/master/install
 bash ./zsh_installer
-wget -O zinit.sh https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh
-bash ./zinit.sh
 cd -
--- rc/mybash.sh
+++ rc/mybash.sh
@@ -3,14 +3,4 @@
 source $USER_CUSTOM/rc/myset.sh
 
 # PS1
-parse_ip() {
-    ip route get 1.1.1.1 | awk -F"src " 'NR == 1{ split($2, a," ");print a[1]}'
-}
-parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-braket=$(tput setaf 82)
-workdir=$(tput setaf 33)
-gitbranch=$(tput setaf 226)
-reset=$(tput sgr0)
-PS1="\[$braket\][\$(parse_ip)\[$braket\]]:\[$workdir\]\w\[$gitbranch\]\$(parse_git_branch)\n\u\[$reset\]$ ";
+export PS1=$FALLBACK_PS1

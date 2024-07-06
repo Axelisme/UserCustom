0a32bf593dbe02a568490d9bbc33014f4b581809
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-06 11:35:42 +0800

    Refactor the package installation script to use the appropriate package manager based on availability.

---
+++ install_arch_dependencies.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+NEED_PACKAGE='xclip nnn tmux zoxide git lsd fzf oh-my-posh-bin neovim'
+
+if type paru > /dev/null 2>&1; then
+    paru -S --needed $NEED_PACKAGE;
+elif type yay > /dev/null 2>&1; then
+    yay -S --needed $NEED_PACKAGE;
+else
+    echo "No aur package manager found";
+fi
--- paru_all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-NEED_PACKAGE='xclip nnn tmux zoxide gvim git lsd fzf oh-my-posh-bin neovim'
-paru -S --needed $NEED_PACKAGE

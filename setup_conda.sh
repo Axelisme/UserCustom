d1d09a8df0756a664ee9cd7987d1ab531c5d8610
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-15 22:53:09 +0800

    check for conda and micromamba availability before executing

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# if conda is not installed, show a warning and exit
+if ! type conda > /dev/null 2>&1; then
+    echo "conda could not be found. Please install conda and try again."
+    exit
+fi
+
 [[ -f $HOME/.bashrc ]] && conda init bash;
 [[ -f $HOME/.zshrc ]] && conda init zsh;
 bash -i -c 'conda config --set auto_activate_base false';
--- setup_mamba.sh
+++ setup_mamba.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# if micromamba is not installed, show a warning and exit
+if ! type micromamba > /dev/null 2>&1; then
+    echo "micromamba could not be found. Please install micromamba and try again."
+    exit
+fi
+
 [[ -f $HOME/.bashrc ]] && micromamba shell init -s bash;
 [[ -f $HOME/.zshrc ]] && micromamba shell init -s zsh;

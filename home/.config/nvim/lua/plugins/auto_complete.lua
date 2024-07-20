87d073262752ec87f89b040d9f44569094c11df0
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-20 21:27:45 +0800

    add copilot to nvim

---
+++ home/.config/nvim/lua/plugins/auto_complete.lua
@@ -0,0 +1,79 @@
+return {
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    -- lazy = false,
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = "<C-Up>",
+          accept_word = "<C-Right>",
+          accept_line = "<C-Down>",
+          dismiss = "<C-Left>",
+        },
+      },
+    },
+  },
+
+  -- copilot icon
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local colors = {
+        [""] = LazyVim.ui.fg("Special"),
+        ["Normal"] = LazyVim.ui.fg("Special"),
+        ["Warning"] = LazyVim.ui.fg("DiagnosticError"),
+        ["InProgress"] = LazyVim.ui.fg("DiagnosticWarn"),
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local icon = LazyVim.config.icons.kinds.Copilot
+          local status = require("copilot.api").status.data
+          return icon .. (status.message or "")
+        end,
+        cond = function()
+          if not package.loaded["copilot"] then
+            return
+          end
+          local ok, clients = pcall(LazyVim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+          if not ok then
+            return false
+          end
+          return ok and #clients > 0
+        end,
+        color = function()
+          if not package.loaded["copilot"] then
+            return
+          end
+          local status = require("copilot.api").status.data
+          return colors[status.status] or colors[""]
+        end,
+      })
+    end,
+  },
+
+  -- override source of nvim-cmp for above line menu
+  {
+    "llllvvuu/nvim-cmp",
+    branch = "feat/above",
+    -- enabled = false,
+    opts = {
+      view = {
+        entries = { vertical_positioning = "above", follow_cursor = true },
+      },
+      experimental = { ghost_text = false },
+    },
+  },
+
+  -- disable snippets
+  { "garymjr/nvim-snippets", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+}

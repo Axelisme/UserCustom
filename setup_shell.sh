5c27ded4ec1073175b9e794899e545fd87eda20f
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-21 21:10:07 +0800

    fix setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
 # recursive copy, if file exists, backup it
-backup_apply() {                               # backup_apply command src dst flag
+backup_apply() { # backup_apply command src dst flag
   local cmd=$1 src=$2 dst=$3 flag=$4
 
-  if [[ ! -e $src ]]; then                    # if src not exists
+  if [[ ! -e $src ]]; then # if src not exists
     echo "Error: $src not exists"
-  elif [[ -d $src ]]; then                    # if src is a directory
-    if [[ -e $dst ]]; then                  # if dst exists
-      if [[ $src -ef $dst ]]; then        # if src and dst are the same
+  elif [[ -d $src ]]; then         # if src is a directory
+    if [[ -e $dst ]]; then         # if dst exists
+      if [[ $src -ef $dst ]]; then # if src and dst are the same
         echo "Skip $dst"
         return
-      elif [[ ! -d $dst ]]; then          # if dst is not a directory
+      elif [[ ! -d $dst ]]; then # if dst is not a directory
         echo "Backup $dst"
         mv $dst $dst.bak
       fi
+    fi
+    mkdir -p $dst # create dst directory
+    for file in $(\ls -A $src); do
+      backup_apply $1 $src/$file $dst/$file
+    done
+  elif [[ -f $src ]]; then         # if src is a file
+    if [[ -e $dst ]]; then         # if dst exists
+      if [[ $src -ef $dst ]]; then # if src and dst are the same
+        echo "Skip $dst"
+        return
       fi
-      mkdir -p $dst                           # create dst directory
-      for file in $(\ls -A $src); do
-        backup_apply $1 $src/$file $dst/$file
-      done
-    elif [[ -f $src ]]; then                    # if src is a file
-      if [[ -e $dst ]]; then                  # if dst exists
-        if [[ $src -ef $dst ]]; then        # if src and dst are the same
-          echo "Skip $dst"
-          return
-        fi
-        echo "Backup $dst"
-        mv --backup=numbered $dst $dst.bak  # backup dst
-        fi
-        echo "Apply $cmd on $dst"
-        $cmd $flag $src $dst
-      fi
-    }
+      echo "Backup $dst"
+      mv --backup=numbered $dst $dst.bak # backup dst
+    fi
+    echo "Apply $cmd on $dst"
+    $cmd $flag $src $dst
+  fi
+}
 
-    UserCustom=$(dirname "$0")
-#    backup_apply cp $UserCustom/home/.bashrc ~/.bashrc  # setup bashrc
-#    backup_apply cp $UserCustom/home/.zshrc  ~/.zshrc   # setup zshrc
-#    backup_apply ln $UserCustom/home/.vimrc  ~/.vimrc   # setup vimrc
-    backup_apply ln $UserCustom/home/.config ~/.config  # setup config
+UserCustom=$(dirname "$0")
+backup_apply cp $UserCustom/home/.bashrc ~/.bashrc # setup bashrc
+backup_apply cp $UserCustom/home/.zshrc ~/.zshrc   # setup zshrc
+backup_apply ln $UserCustom/home/.vimrc ~/.vimrc   # setup vimrc
+backup_apply ln $UserCustom/home/.config ~/.config # setup config
 
-    unset backup_ln
+unset backup_ln

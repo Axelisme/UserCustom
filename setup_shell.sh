f72c6ebffd0a9b738296ee0bf9d59e025faa3724
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-21 22:00:16 +0800

    Use softlink instead of hard link to setup .config

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,43 +1,31 @@
 #!/usr/bin/env bash
 
-# recursive copy, if file exists, backup it
-backup_apply() { # backup_apply command src dst flag
-  local cmd=$1 src=$2 dst=$3 flag=$4
-
-  if [[ ! -e $src ]]; then # if src not exists
-    echo "Error: $src not exists"
-  elif [[ -d $src ]]; then         # if src is a directory
-    if [[ -e $dst ]]; then         # if dst exists
-      if [[ $src -ef $dst ]]; then # if src and dst are the same
-        echo "Skip $dst"
-        return
-      elif [[ ! -d $dst ]]; then # if dst is not a directory
-        echo "Backup $dst"
-        mv $dst $dst.bak
-      fi
-    fi
-    mkdir -p $dst # create dst directory
-    for file in $(\ls -A $src); do
-      backup_apply $1 $src/$file $dst/$file
-    done
-  elif [[ -f $src ]]; then         # if src is a file
-    if [[ -e $dst ]]; then         # if dst exists
-      if [[ $src -ef $dst ]]; then # if src and dst are the same
-        echo "Skip $dst"
-        return
-      fi
-      echo "Backup $dst"
-      mv --backup=numbered $dst $dst.bak # backup dst
-    fi
-    echo "Apply $cmd on $dst"
-    $cmd $flag $src $dst
-  fi
-}
-
 UserCustom=$(dirname "$0")
-backup_apply cp $UserCustom/home/.bashrc ~/.bashrc # setup bashrc
-backup_apply cp $UserCustom/home/.zshrc ~/.zshrc   # setup zshrc
-backup_apply ln $UserCustom/home/.vimrc ~/.vimrc   # setup vimrc
-backup_apply ln $UserCustom/home/.config ~/.config # setup config
 
-unset backup_ln
+cp -b $UserCustom/home/.bashrc ~/.bashrc # setup bashrc
+cp -b $UserCustom/home/.zshrc ~/.zshrc   # setup zshrc
+ln -b $UserCustom/home/.vimrc ~/.vimrc   # setup vimrc
+
+# setup config files
+for src in $(realpath $UserCustom/home/.config/*); do
+  dst=$HOME/.config/$(basename $src)
+
+  # if dst exists and not a link, backup it
+  if [ -e $dst ]; then
+    if [ ! -L $dst ]; then
+      echo "backup $dst"
+      mv -b $dst $dst.bak
+    else
+      echo "skip $dst"
+    fi
+  fi
+
+  # link src to dst
+  if [ -d $src ]; then # is directory
+    # soft link
+    ln -s $src $dst
+  else # is file
+    # hard link
+    ln $src $dst
+  fi
+done

6a7f5c5473c0acf2d555d0cbe51990be77fba1f5
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-07-28 20:39:12 +0800

    silent kitty and auto close no name buffer

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,6 @@ return {
   keys = {
     { "<leader><space>", "<cmd>Telescope find_files<CR>", desc = "Find Files (cwd)" },
     { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files (cwd)" },
-    { "<leader>fF", false },
+    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
   },
 }

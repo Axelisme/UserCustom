735bafb5c77235f4ee2fa1d621c22f8b9d4dcf62
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-08-11 13:40:09 +0800

    fix install_mamba script

--- install_scripts/install_mamba.sh
+++ install_scripts/install_mamba.sh
@@ -3,8 +3,8 @@
 if ! command -v mamba &> /dev/null; then
     echo "installing mamba"
     mkdir -p ~/.local/bin
-    cd ~/.local/bin
-    curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj micromamba
+    cd ~/.local
+    curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
     cd -
 else
     echo "mamba already installed"

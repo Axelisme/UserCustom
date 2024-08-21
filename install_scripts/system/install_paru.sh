5287e640bb366af5814714425e51b13ae0235451
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-08-21 16:30:25 +0800

    add script to install yay and paru

---
+++ install_scripts/system/install_paru.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# command name to installed
+name="paru"
+
+# main function to install the command
+install_function() {
+  cd /tmp
+
+  sudo pacman -S --needed base-devel
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
+}
+
+# wrapper to install the command
+if ! command -v $name &>/dev/null; then
+  echo -n "installing $name..."
+  install_function >/dev/null
+  echo "done"
+else
+  echo "$name already installed, skipping"
+fi
---
+++ install_scripts/system/install_yay.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# command name to installed
+name="yay"
+
+# main function to install the command
+install_function() {
+  cd /tmp
+
+  pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+}
+
+# wrapper to install the command
+if ! command -v $name &>/dev/null; then
+  echo -n "installing $name..."
+  install_function >/dev/null
+  echo "done"
+else
+  echo "$name already installed, skipping"
+fi

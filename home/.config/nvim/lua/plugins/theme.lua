d8d6c0fabd5213eb3d5acefc9ffe16d20518e325
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-02 15:31:13 +0800

    change nvim theme and remove command-not-found zsh plugin

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,9 @@
 return {
-  { "olimorris/onedarkpro.nvim", priority = 1000 },
-  { "catppuccin", enabled = false },
-
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark_vivid",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }

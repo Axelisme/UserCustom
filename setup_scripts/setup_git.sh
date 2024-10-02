3d9a1b0c362e0e30df33923923fdc1d55b965936
stephenjkhsieh <stephen.jk.hsieh@gmail.com>
2024-10-03 02:08:33 +0800

    Refactor git configuration script don't use ssh instead of https

--- setup_scripts/setup_git.sh
+++ setup_scripts/setup_git.sh
@@ -5,4 +5,3 @@ read -p 'Email: ' emailvar
 
 git config --global user.name   "$uservar" ;
 git config --global user.email  "$emailvar" ;
-git config --global url.ssh://git@github.com/.insteadOf https://github.com/;

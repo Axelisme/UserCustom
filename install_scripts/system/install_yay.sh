6dd43e16d4248a5ef76922aecd8a40750700c7dd
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-26 18:05:16 +0800

    chmod install_yay/paru to excutable

--- install_scripts/system/install_yay.sh
+++ install_scripts/system/install_yay.sh
@@ -7,7 +7,7 @@ name="yay"
 install_function() {
   cd /tmp
 
-  pacman -S --needed git base-devel
+  sudo pacman -S --needed git base-devel
   git clone https://aur.archlinux.org/yay.git
   cd yay
   makepkg -si

e78960861e184a556be58d143b19d69e685e5a1f
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-27 14:26:12 +0800

    Fix copilot suggestion invisible color

--- home/.config/nvim/lua/plugins/theme.lua
+++ home/.config/nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
   { "catppuccin", enabled = false },
   {
     "olimorris/onedarkpro.nvim",
-    opts = { colors = { onedark_dark = { bg = "#202020" } } },
+    opts = { colors = { onedark_dark = { bg = "#202020", gray = "#6b6b6b" } } },
   },
 
   -- Configure LazyVim to load colorscheme

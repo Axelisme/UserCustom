196de80febad658b291d990db53e086713936608
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-28 21:52:57 +0800

    Replace copilot icon plugin

--- home/.config/nvim/lua/plugins/auto_complete.lua
+++ home/.config/nvim/lua/plugins/auto_complete.lua
@@ -3,7 +3,8 @@ return {
   {
     "zbirenbaum/copilot.lua",
     -- enabled = false,
-    event = "InsertEnter",
+    -- event = "InsertEnter",
+    event = { "BufReadPre", "BufNewFile" },
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
@@ -21,51 +22,23 @@ return {
   },
 
   -- copilot icon
+  { "AndreM222/copilot-lualine", event = "VeryLazy" },
   {
     "nvim-lualine/lualine.nvim",
     optional = true,
-    event = "VeryLazy",
     opts = function(_, opts)
-      local colors = {
-        [""] = LazyVim.ui.fg("Special"),
-        ["Normal"] = LazyVim.ui.fg("Special"),
-        ["Warning"] = LazyVim.ui.fg("DiagnosticError"),
-        ["InProgress"] = LazyVim.ui.fg("DiagnosticWarn"),
-      }
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = LazyVim.config.icons.kinds.Copilot
-          local status = require("copilot.api").status.data
-          return icon .. (status.message or "")
-        end,
-        cond = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local ok, clients = pcall(LazyVim.lsp.get_clients, { name = "copilot", bufnr = 0 })
-          if not ok then
-            return false
-          end
-          return ok and #clients > 0
-        end,
-        color = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local status = require("copilot.api").status.data
-          return colors[status.status] or colors[""]
-        end,
-      })
+      table.insert(opts.sections.lualine_x, 2, { "copilot", symbols = { show_colors = true } })
     end,
   },
 
   -- completion
   {
     "hrsh7th/nvim-cmp",
-    enabled = false,
+    -- enabled = false,
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.experimental = { ghost_text = false }
+      opts.view = { entries = { name = "custom", selection_order = "bottom_up" } }
 
       local toggle_menu = function()
         if cmp.visible() then
@@ -83,6 +56,32 @@ return {
         end
       end
 
+      local move_down = function(fallback)
+        if cmp.visible() then
+          ---@diagnostic disable-next-line: invisible
+          if cmp.core.view.custom_entries_view:is_direction_top_down() then
+            cmp.select_next_item()
+          else
+            cmp.select_prev_item()
+          end
+        else
+          fallback()
+        end
+      end
+
+      local move_up = function(fallback)
+        if cmp.visible() then
+          ---@diagnostic disable-next-line: invisible
+          if cmp.core.view.custom_entries_view:is_direction_top_down() then
+            cmp.select_prev_item()
+          else
+            cmp.select_next_item()
+          end
+        else
+          fallback()
+        end
+      end
+
       opts.mapping = {
         -- disable follow keymap
         ["<Down>"] = cmp.config.disable,
@@ -90,8 +89,10 @@ return {
         ["<CR>"] = cmp.config.disable,
         ["<C-Space>"] = cmp.config.disable,
         -- set new keymap
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        -- ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        -- ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-n>"] = cmp.mapping(move_down, { "i", "s" }),
+        ["<C-p>"] = cmp.mapping(move_up, { "i", "s" }),
         ["<C-y>"] = cmp.mapping(accept_completion, { "i", "s" }),
         ["<S-CR>"] = cmp.mapping(accept_completion, { "i", "s" }),
         ["<C-b>"] = cmp.mapping(toggle_menu, { "i", "s" }),
@@ -102,14 +103,14 @@ return {
       return {}
     end,
   },
-  {
-    "llllvvuu/nvim-cmp",
-    branch = "feat/above",
-    enabled = true,
-    opts = {
-      view = {
-        entries = { vertical_positioning = "above", follow_cursor = true },
-      },
-    },
-  },
+  -- {
+  --   "llllvvuu/nvim-cmp",
+  --   branch = "feat/above",
+  --   enabled = true,
+  --   opts = {
+  --     view = {
+  --       entries = { vertical_positioning = "above", follow_cursor = true },
+  --     },
+  --   },
+  -- },
 }

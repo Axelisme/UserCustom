3dd87820f8c4ce0e49eddf8307f7905eaebcd6ce
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-31 02:29:13 +0800

    change functionality of <Tab> keymap in neo-tree

--- home/.config/nvim/lua/plugins/neo-tree.lua
+++ home/.config/nvim/lua/plugins/neo-tree.lua
@@ -12,12 +12,11 @@ return {
       window = {
         mappings = {
           ["<Tab>"] = function(state)
-            local node = state.tree:get_node()
-            if require("neo-tree.utils").is_expandable(node) then
-              state.commands["toggle_node"](state)
-            else
-              state.commands["open"](state)
-              vim.cmd("Neotree reveal")
+            local current_node = state.tree:get_node() -- this is the current node
+            local path = current_node:get_id() -- this gives you the path
+
+            if vim.fn.isdirectory(path) == 1 then
+              require("neo-tree.command").execute({ dir = path })
             end
           end,
           ["."] = function(state)

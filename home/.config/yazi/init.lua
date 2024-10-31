09be515cd1c009e9344372150a7d0ea81d344fc0
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-10-31 17:12:47 +0800

    show link in yazi bar instead of folder

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -15,3 +15,18 @@ Status:children_add(function()
 		ui.Span(" "),
 	})
 end, 500, Status.RIGHT)
+
+-- show ln in status-bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+
+	-- return ui.Line(" " .. h.name)
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end

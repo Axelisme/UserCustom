c50c9c187232e8b0e390e1b64a0bd429888fb047
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-05 00:11:33 +0800

    use default lualine.nvim sections

--- home/.config/nvim/lua/plugins/02_lualine.lua
+++ home/.config/nvim/lua/plugins/02_lualine.lua
@@ -1,35 +1,9 @@
-local icons = LazyVim.config.icons
-
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { LazyVim.lualine.pretty_path() },
-        lualine_x = {
-          "%S",
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_y = { { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 1 } } },
+        lualine_y = {},
         lualine_z = {},
       },
     },

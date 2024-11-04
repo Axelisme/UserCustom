fdf4122f76390321812bea4dd3ea72f31fa9d300
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-05 00:58:10 +0800

    do not show buffer and git status in neo-tree

--- home/.config/nvim/lua/plugins/edgy.lua
+++ home/.config/nvim/lua/plugins/edgy.lua
@@ -1,8 +1,12 @@
 return {
   "folke/edgy.nvim",
-  opts = {
-    animate = {
-      enabled = false,
-    },
-  },
+  opts = function(_, opts)
+    opts.animate = { enabled = false }
+    opts.wo = { winbar = false }
+
+    -- disable show buffer and git status in neovim
+    opts.left = vim.tbl_filter(function(panel)
+      return panel.title == "Neo-Tree Filesystem"
+    end, opts.left)
+  end,
 }

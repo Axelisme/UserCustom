6d83d473766ddb2cd1a3703fb3cca05a121d2ff6
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-09 02:57:33 +0800

    update to LazyVim 13.x

--- home/.config/nvim/lua/plugins/edgy.lua
+++ home/.config/nvim/lua/plugins/edgy.lua
@@ -2,7 +2,7 @@ return {
   "folke/edgy.nvim",
   opts = function(_, opts)
     opts.animate = { enabled = false }
-    opts.wo = { winbar = false }
+    opts.wo = { winbar = "" }
 
     -- disable show buffer and git status in neovim
     opts.left = vim.tbl_filter(function(panel)

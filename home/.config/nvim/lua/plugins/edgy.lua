29005e1c49d5d2fd13dd5e9b79d6ab137a1a9044
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-12 03:26:49 +0800

    add key to resize terminal in nvim terminal mode

--- home/.config/nvim/lua/plugins/edgy.lua
+++ home/.config/nvim/lua/plugins/edgy.lua
@@ -1,5 +1,10 @@
 return {
   "folke/edgy.nvim",
+  keys = {
+    -- resize windows in terminal mode
+    { "<C-Up>", "<cmd>lua require('edgy').get_win():resize('height', 2)<CR>", mode = "t" },
+    { "<C-Down>", "<cmd>lua require('edgy').get_win():resize('height', -2)<CR>", mode = "t" },
+  },
   opts = function(_, opts)
     opts.animate = { enabled = false }
     opts.wo = { winbar = "" }

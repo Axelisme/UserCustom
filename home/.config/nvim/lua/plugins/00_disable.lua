bba680a8c9691d66b2a0b3391af9c2289238b8cd
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-14 00:48:30 +0800

    enable flash.nvim

--- home/.config/nvim/lua/plugins/00_disable.lua
+++ home/.config/nvim/lua/plugins/00_disable.lua
@@ -3,7 +3,7 @@ return {
   { "ts-comments.nvim", enabled = false },
   { "tokyonight.nvim", enabled = false },
   { "nvim-ts-autotag", enabled = false },
-  { "flash.nvim", enabled = false },
+  -- { "flash.nvim", enabled = false },
   -- { "nvim-snippets", enabled = false },
   -- { "rafamadriz/friendly-snippets", enabled = false },
   -- { "luvit-meta", enabled = false },

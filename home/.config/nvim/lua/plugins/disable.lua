06f739e2022c2b0691ef5686a7a4136da4e935d4
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-14 13:57:56 +0800

    merge with upstream commits

--- home/.config/nvim/lua/plugins/disable.lua
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -4,7 +4,7 @@ return {
   { "ts-comments.nvim", enabled = false },
   { "tokyonight.nvim", enabled = false },
   { "nvim-ts-autotag", enabled = false },
-  { "flash.nvim", enabled = false },
+  -- { "flash.nvim", enabled = false },
   -- { "nvim-snippets", enabled = false },
   -- { "rafamadriz/friendly-snippets", enabled = false },
   -- { "luvit-meta", enabled = false },
--- home/.config/nvim/lua/plugins/edgy.lua
+++ home/.config/nvim/lua/plugins/edgy.lua
@@ -5,17 +5,6 @@ return {
     { "<C-Up>", "<cmd>lua require('edgy').get_win():resize('height', 2)<CR>", mode = "t" },
     { "<C-Down>", "<cmd>lua require('edgy').get_win():resize('height', -2)<CR>", mode = "t" },
   },
-  init = function()
-    -- fix the terminal window not close in first time
-    vim.api.nvim_create_autocmd("TermClose", {
-      group = vim.api.nvim_create_augroup("term_close", { clear = true }),
-      once = true,
-      callback = function()
-        -- close terminal window in default way
-        vim.cmd("q")
-      end,
-    })
-  end,
   opts = function(_, opts)
     opts.animate = { enabled = false }
     opts.wo = { winbar = "" }

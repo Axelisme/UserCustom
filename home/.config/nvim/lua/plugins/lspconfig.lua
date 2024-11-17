523d6a55bdfb8196f0430b61777fb6f980fb3457
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-17 23:53:18 +0800

    fix signature help cover current line issue

--- home/.config/nvim/lua/plugins/lspconfig.lua
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,11 +5,21 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable <c-k> for navigate in insert mode
       keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-      keys[#keys + 1] = { "<c-i>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", mode = "i" }
       -- disable <Tab> for indent in insert mode
       keys[#keys + 1] = { "<Tab>", false, mode = "i" }
 
       opts.inlay_hints = { enabled = false }
     end,
+  },
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = {
+      lsp = {
+        signature = {
+          opts = { size = { max_height = math.floor(vim.api.nvim_win_get_height(0) / 2) } },
+        },
+      },
+    },
   },
 }

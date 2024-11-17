b587ecb7bd22b896f2761d90ce59042e2a1fa70e
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-17 23:52:56 +0800

    add short-cut to molten-nvim

--- home/.config/nvim/lua/plugins/lang/python.lua
+++ home/.config/nvim/lua/plugins/lang/python.lua
@@ -18,11 +18,6 @@ return {
             type = "anaconda",
           },
         },
-        options = {
-          debug = true,
-          activate_venv_in_terminal = true,
-          set_environment_variables = true,
-        },
       },
     },
   },
@@ -75,12 +70,10 @@ return {
   {
     "benlubas/molten-nvim",
     build = ":UpdateRemotePlugins",
-    cmd = "MoltenInit",
     dependencies = { "image.nvim" },
     keys = {
-      { "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" } },
-      { "<localleader>ml", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Evaluate line" } },
-      { "<localleader>ms", ":<C-u>MoltenEvaluateVisual<CR>gv<esc>", { silent = true, desc = "Evaluate selection" } },
+      { "<localleader>mi", "<cmd>MoltenInit<CR>", desc = "Initialize the plugin" },
+      { "<localleader>ml", "<cmd>MoltenEvaluateLine<CR>", desc = "Evaluate line" },
     },
     config = function()
       vim.g.molten_image_provider = "image.nvim"
@@ -92,6 +85,72 @@ return {
       map("n", "<localleader>md", ":MoltenDelete<CR>", "Delete cell")
       map("n", "<localleader>mh", ":MoltenHideOutput<CR>", "Hide output")
       map("n", "<localleader>mo", ":noautocmd MoltenEnterOutput<CR>", "Open output")
+      map("n", "<S-CR>", function()
+        local function is_cell_delimiter(bufnr, line_num)
+          local line = vim.api.nvim_buf_get_lines(bufnr, line_num, line_num + 1, false)[1]
+          return line:find("# %%") ~= nil
+        end
+        -- Helper function to check if a line is non-empty
+        local function is_non_empty_line(bufnr, line_num)
+          local line = vim.api.nvim_buf_get_lines(bufnr, line_num, line_num + 1, false)[1]
+          return line:find("%S") ~= nil
+        end
+        -- Find the start of the current cell
+        local function find_cell_start(bufnr, current_line)
+          local start_line = current_line
+
+          while start_line > 0 do
+            if is_cell_delimiter(bufnr, start_line) then
+              return start_line
+            end
+            start_line = start_line - 1
+          end
+
+          return nil
+        end
+        -- Find the end of the current cell
+        local function find_cell_end(bufnr, start_line)
+          local end_line = start_line
+          local last_non_empty = start_line
+          local max_line = vim.api.nvim_buf_line_count(bufnr) - 1
+
+          while end_line < max_line do
+            end_line = end_line + 1
+
+            if is_non_empty_line(bufnr, end_line) then
+              last_non_empty = end_line
+            end
+
+            if is_cell_delimiter(bufnr, end_line) then
+              return last_non_empty
+            end
+          end
+
+          return last_non_empty
+        end
+        local original_pos = vim.api.nvim_win_get_cursor(0)
+        local current_line = original_pos[1] - 1
+
+        -- Find cell boundaries
+        local start_line = find_cell_start(0, current_line)
+        if not start_line then
+          Snacks.notify("No cell delimiter found before cursor")
+          return
+        end
+
+        local end_line = find_cell_end(0, start_line)
+
+        if start_line == end_line then
+          Snacks.notify("No cell delimiter found after cursor")
+          return
+        end
+
+        -- Move cursor and make selection
+        vim.api.nvim_win_set_cursor(0, { start_line + 1, 0 })
+        vim.cmd(string.format("normal! V%dG", end_line + 1))
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", false, true, true), "nx", false)
+        vim.cmd("MoltenEvaluateVisual")
+      end, "Evaluate Block")
     end,
   },
 }

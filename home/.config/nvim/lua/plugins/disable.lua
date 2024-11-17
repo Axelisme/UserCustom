58bf46ce7d38c5214075b07d9556fae8b891d7fb
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-11-17 23:52:34 +0800

    remove rust config and do some refactor

--- home/.config/nvim/lua/plugins/disable.lua
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -1,13 +1,7 @@
 return {
-  { "b0o/SchemaStore.nvim", enabled = false },
+  { "SchemaStore.nvim", enabled = false },
   { "catppuccin", enabled = false },
   { "ts-comments.nvim", enabled = false },
   { "tokyonight.nvim", enabled = false },
   { "nvim-ts-autotag", enabled = false },
-  -- { "flash.nvim", enabled = false },
-  -- { "nvim-snippets", enabled = false },
-  -- { "rafamadriz/friendly-snippets", enabled = false },
-  -- { "luvit-meta", enabled = false },
-  -- { "nvim-treesitter-textobjects", enabled = false },
-  -- { "nvim-lint", enabled = false },
 }

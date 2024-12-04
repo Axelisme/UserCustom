c19124c49f350a3f6a7e99aedcdadb02a379c861
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-12-04 23:22:51 +0800

    change some keymap setting

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -2,14 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Let <C-c> trigger "InsertEnter" event
-vim.keymap.set("i", "<C-c>", "<Esc>", { remap = true })
-
--- <Enter> in normal mode to insert newline below
-vim.keymap.set("n", "<Enter>", "o<Esc>", { remap = true })
-
--- Let <C-c> trigger "InsertEnter" event
-vim.keymap.set("i", "<C-c>", "<Esc>", { remap = true })
+-- Let <C-l> work in terminal event
+vim.keymap.set("t", "<C-l>", "<C-l>", { remap = true })
 
 -- Navigate in insert mode by Ctrl-hjkl
 vim.keymap.set("i", "<c-h>", "<Left>")
@@ -18,4 +12,4 @@ vim.keymap.set("i", "<c-k>", "<Up>")
 vim.keymap.set("i", "<c-l>", "<Right>")
 
 -- cd to the directory of the current file
-vim.keymap.set("n", "<leader>.", ":cd %:p:h<CR>:pwd<CR>", { desc = "Cd to current file directory" })
+vim.keymap.set("n", "<leader>;", ":cd %:p:h<CR>:pwd<CR>", { desc = "Cd to current file directory" })

a62f437155c64def81854f67aec396ebddb90da9
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2024-12-04 23:22:18 +0800

    enable c-l in neweset snacks

---
+++ home/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      win = {
+        keys = {
+          ["<C-L>"] = false, -- let <C-L> clean terminal in terminal mode
+        },
+      },
+    },
+  },
+}

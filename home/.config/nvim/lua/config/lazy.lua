92d6b73dedea1b830a76f8eba2364219d2a22e9e
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2025-01-29 00:37:22 +0800

    swich to snacks.picker

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins.lang" },

ad6d547ac521556819e83211e82cd42b53920240
Axelisme <jary33333@gmail.com>
2025-04-25 12:14:16 +0800

    add vscode keymap to neovim

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -13,3 +13,45 @@ vim.keymap.set("i", "<c-l>", "<Right>")
 
 -- cd to the directory of the current file
 vim.keymap.set("n", "<leader>;", ":cd %:p:h<CR>:pwd<CR>", { desc = "Cd to current file directory" })
+
+-- vscode
+if vim.g.vscode then -- VSCode extension
+  local vscode = require("vscode-neovim")
+  local function map(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, function()
+      vscode.call(rhs)
+    end, { silent = true, noremap = true })
+  end
+
+  -- Remap folding keys
+  map("n", "zM", "editor.foldAll")
+  map("n", "zR", "editor.unfoldAll")
+  map("n", "zc", "editor.fold")
+  map("n", "zC", "editor.foldRecursively")
+  map("n", "zo", "editor.unfold")
+  map("n", "zO", "editor.unfoldRecursively")
+  map("n", "za", "editor.toggleFold")
+
+  -- Remap move keys
+  local function mapMove(key, direction)
+    -- print("Mapping move key: ", key, direction)
+    vim.keymap.set("n", key, function()
+      local count = vim.v.count
+      local v = 1
+      local style = "wrappedLine"
+      if count > 0 then
+        v = count
+        style = "line"
+      end
+      vscode.action("cursorMove", {
+        args = {
+          to = direction,
+          by = style,
+          value = v,
+        },
+      })
+    end)
+  end
+  mapMove("k", "up")
+  mapMove("j", "down")
+end

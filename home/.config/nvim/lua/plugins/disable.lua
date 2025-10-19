68f2d37f43489e89700b1fd84b1d1aba24f09865
Axelisme <jary33333@gmail.com>
2025-10-19 15:01:03 +0800

    enable SchemaStore.nvim

--- home/.config/nvim/lua/plugins/disable.lua
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -1,7 +1,6 @@
 return {
   { "catppuccin", enabled = false },
   { "tokyonight.nvim", enabled = false },
-  { "SchemaStore.nvim", enabled = false },
   { "nvim-ts-autotag", enabled = false },
   { "ts-comments.nvim", enabled = false },
 }

dc2394af6a6b8f3f3003038f792a2385abe6b0bc
Axelisme <jary33333@gmail.com>
2025-11-27 00:40:03 +0800

    update neovim config

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -19,30 +19,6 @@ autocmd("BufEnter", {
   desc = "Don't new line comments",
 })
 
--- -- no No Name Buffer
--- autocmd({ "BufReadPre", "BufReadPost" }, {
---   pattern = { "*" },
---   callback = function()
---     local bufs = vim.fn.getbufinfo({ buflisted = 1 })
---     for _, buf in pairs(bufs) do
---       -- If the buffer is unnamed
---       if buf.name == "" then
---         local lines = vim.api.nvim_buf_get_lines(buf.bufnr, 0, -1, false)
---         local num_chars = 0
---         for _, line in pairs(lines) do
---           num_chars = num_chars + #line
---         end
---         -- If the buffer is empty
---         if num_chars == 0 then
---           vim.api.nvim_buf_delete(buf.bufnr, { force = true })
---         end
---       end
---     end
---   end,
---   group = augroup("no_no_name_buffer", { clear = true }),
---   desc = "no No Name Buffer",
--- })
-
 -- Enable <C-l> in terminal
 autocmd("TermOpen", {
   desc = "Enable <C-l> in terminal",

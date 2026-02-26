7836547addcdbebd6c25a4a104634e431d483447
Axelisme <jary33333@gmail.com>
2026-02-26 13:38:22 +0800

    add ftplugin for nvim

---
+++ home/.config/nvim/after/ftplugin/c.vim
@@ -0,0 +1,4 @@
+setlocal tabstop=2 " A TAB character looks like 4 spaces
+setlocal expandtab " Pressing the TAB key will insert spaces instead of a TAB character
+setlocal softtabstop=2 " Number of spaces inserted instead of a TAB character
+setlocal shiftwidth=2 " Number of spaces inserted when indenting
--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -12,8 +12,3 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 4
 
 vim.g.ai_cmp = false
-
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
